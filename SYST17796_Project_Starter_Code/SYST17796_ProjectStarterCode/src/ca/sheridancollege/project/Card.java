10217417663eaf0ab18db826d7b3c8fd998271b7
Justin <marsellj@sheridancollege.ca>
2019-02-15 14:21:15 -0500

    My code that I am supposed to edit

--- SYST17796_Project_Starter_Code/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Card.java
+++ SYST17796_Project_Starter_Code/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Card.java
@@ -10,6 +10,7 @@ package ca.sheridancollege.project;
  * enough to be instantiated for any Card game. Students wishing to add to the code 
  * should remember to add themselves as a modifier.
  * @author dancye, 2018
+ * @Ultimate_Scrub_Lord not Justin
  */
 public abstract class Card 
 {
